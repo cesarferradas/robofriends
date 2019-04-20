bd990578e1d0e557ce7aaf3778a521e0c6a88211
Cesar Ferradas <cmfv96@gmail.com>
2019-04-20 13:27:33 +0100

    add initial robots app files

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Dan Codd',
+    username: 'dancodd',
+    email: 'dan.codd@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Cesar Ferradas',
+    username: 'cesarf',
+    email: 'cmfv96@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Valerie Wong',
+    username: 'vwong1',
+    email: 'val.wong@wong.pe'
+  },
+  {
+    id: 4,
+    name: 'Michael Jackson',
+    username: 'mikej5',
+    email: 'mike@jackson5.com'
+  },
+  {
+    id: 5,
+    name: 'John Smith',
+    username: 'jsmith',
+    email: 'jsmith@gmail.com'
+  },
+]
