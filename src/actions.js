4cfaaa2a609154bd284b80ae713df259e21a94b6
Cesar Ferradas <cmfv96@gmail.com>
2019-04-22 21:48:36 +0100

    call API request asynchronously

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,20 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import {
+  CHANGE_SEARCH_FIELD,
+  FETCH_ROBOTS_PENDING,
+  FETCH_ROBOTS_SUCCESS,
+  FETCH_ROBOTS_FAILED
+} from './constants';
 
 
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text
 });
+
+export const fetchRobots = () => (dispatch) => {
+  dispatch({ type: FETCH_ROBOTS_PENDING });
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res) => res.json())
+    .then((robots) => dispatch({ type: FETCH_ROBOTS_SUCCESS, payload: robots }))
+    .catch((error) => dispatch({ type: FETCH_ROBOTS_FAILED, payload: error }));
+}
