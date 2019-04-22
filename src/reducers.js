786d2c5bc8c4e2ac16f80118d7f29328f42d410f
Cesar Ferradas <cesarferradas@pm.me>
2019-04-22 21:48:36 +0100

    call API request asynchronously

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,38 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import {
+  CHANGE_SEARCH_FIELD,
+  FETCH_ROBOTS_PENDING,
+  FETCH_ROBOTS_SUCCESS,
+  FETCH_ROBOTS_FAILED
+} from './constants';
 
 
-const initialState = {
-  searchField: ''
+const initialStateSearch = {
+  searchField: '',
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return { ...state, searchField: action.payload };
+    default:
+      return state;
+  }
+}
+
+const initialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: ''
+}
+
+export const fetchRobots = (state=initialStateRobots, action={}) => {
+  switch (action.type) {
+    case FETCH_ROBOTS_PENDING:
+      return { ...state, isPending: true };
+    case FETCH_ROBOTS_SUCCESS:
+      return { ...state, robots: action.payload, isPending: false };
+    case FETCH_ROBOTS_FAILED:
+      return { ...state, error: action.payload, isPending: false };
     default:
       return state;
   }
