46f964ff264e124de9bced72b45b11a2a20c07a8
cesar <dev@cesar.sh>
2019-05-11 16:24:58 +0100

    Updates

---
+++ precache-manifest.a063eb5db6084ba4749e614662fc8749.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "444aeb0c30f42730f00a",
+    "url": "/robofriends/static/js/main.de0070d4.chunk.js"
+  },
+  {
+    "revision": "fca3dcdf361ef80ae295",
+    "url": "/robofriends/static/js/2.f3ea3424.chunk.js"
+  },
+  {
+    "revision": "444aeb0c30f42730f00a",
+    "url": "/robofriends/static/css/main.b6c6728b.chunk.css"
+  },
+  {
+    "revision": "fca3dcdf361ef80ae295",
+    "url": "/robofriends/static/css/2.e218c9e0.chunk.css"
+  },
+  {
+    "revision": "93011b0e1e45435561b193ccce0fc9e3",
+    "url": "/robofriends/index.html"
+  }
+];
