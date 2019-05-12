426e2862243dd8ecc66d0f5e40918901bac5f303
cesar <dev@cesar.sh>
2019-05-12 12:43:08 +0100

    Updates

--- precache-manifest.43d3133baed1029a532d7c849c51262d.js
+++ precache-manifest.43d3133baed1029a532d7c849c51262d.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
   },
   {
-    "revision": "5509813d0c26df329d6a",
-    "url": "/robofriends/static/js/main.e1d6c386.chunk.js"
+    "revision": "9437374314ca8b7c22b4",
+    "url": "/robofriends/static/js/main.4c3f1c44.chunk.js"
   },
   {
     "revision": "fca3dcdf361ef80ae295",
     "url": "/robofriends/static/js/2.f3ea3424.chunk.js"
   },
   {
-    "revision": "5509813d0c26df329d6a",
-    "url": "/robofriends/static/css/main.b6c6728b.chunk.css"
+    "revision": "9437374314ca8b7c22b4",
+    "url": "/robofriends/static/css/main.2c9fe393.chunk.css"
   },
   {
     "revision": "fca3dcdf361ef80ae295",
     "url": "/robofriends/static/css/2.e218c9e0.chunk.css"
   },
   {
-    "revision": "da8588b950884e4a8eca980f2edfac83",
+    "revision": "f03ddd27febf4267c991f127b2c1ac1b",
     "url": "/robofriends/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.9d99ae6c539f854fb0045c6bc86fe599.js"
+  "/robofriends/precache-manifest.43d3133baed1029a532d7c849c51262d.js"
 );
 
 workbox.clientsClaim();
